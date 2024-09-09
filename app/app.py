de8a29c197e7a5902cf848dc90bd51fce3cd894a
cp29analytics <chris.patrick29@gmail.com>
2024-09-08 23:17:41 -0400

    update selectable paramts

--- app/app.py
+++ app/app.py
@@ -8,9 +8,24 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
+from scipy.optimize import minimize
 
 # Set the title for the Streamlit app
 st.title("Simple Kelly Criterion")
+
+  st.sidebar.header("Select 3  Stocks for your portfolio")
+
+sp500_top50 = [
+    'AAPL', 'MSFT', 'AMZN', 'GOOGL', 'GOOG', 'BRK-B', 'NVDA', 'TSLA', 'META',
+    'UNH', 'JNJ', 'V', 'XOM', 'WMT', 'JPM', 'MA', 'PG', 'HD', 'BAC', 'PFE',
+    'ABBV', 'KO', 'DIS', 'PEP', 'NFLX', 'CSCO', 'NKE', 'LLY', 'MCD', 'VZ',
+    'MRK', 'TMO', 'CVX', 'ABT', 'CMCSA', 'DHR', 'AVGO', 'COST', 'ACN', 'NEE',
+    'WFC', 'LIN', 'TXN', 'ADBE', 'MDT', 'HON', 'UNP', 'ORCL', 'PM'
+]
+# Ticker selection from sp500_top50
+tickers = st.sidebar.multiselect("Select Tickers",
+                                  sp500_top50,
+                                  default=["AAPL", "MSFT", "GOOGL"])
 
 # Create tabs
 tab1, tab2, tab3 = st.tabs([
@@ -40,6 +55,8 @@ with tab1:
   param1 = st.slider("Parameter 1 (Growth multiplier)", min_value=0.1, max_value=3.0, value=1.5, step=0.1)
   param2 = st.slider("Parameter 2 (Loss multiplier)", min_value=0.05, max_value=1.0, value=0.5, step=0.05)
   param3 = st.slider("Parameter 3 (Probability of Success)", min_value=0.05, max_value=1.0, value=0.5, step=0.05)
+  param4 = st.slider("Parameter 4 (Number of Periods)", min_value=10, max_value=1000, value=50, step=10)
+  param5 = st.slider("Parameter 5 (Percent Kelly to Compare)", min_value=0.05, max_value=1.0, value=0.5, step=0.05)
   
   # Generate the data based on user input
   u = np.linspace(0, 1, num=100)
@@ -54,38 +71,65 @@ with tab1:
   plt.ylabel("Expected Growth Rate")
   st.pyplot(fig)
 
+  # lets do an additional exercise by solving for the kelly optimal percent of capital to invest based on the scenario we present
+  # and write a function in python to calculate and plot the full kelly, half kelly, and fully invested strategies, 
+  # similar to the above, but done in a nice python function where inputs are provided
+  def log_util(u, p1, r1, r2):
+    # inverse log utility so we can use the scipy minimize function
+    return -1.0 *(p1*np.log(1+r1*u) + (1-p1)*np.log(1-r2*u))
+
+  def minimize_util(p1, r1, r2):
+    kr = minimize(log_util, 0, args = (p1, r1, r2), method = 'BFGS')
+    return kr.x[0]
+
+  def wealth(p1, r1, r2, n_trades, wealth_1, f):
+    # n is the number of trades, wealth_1 is starting wealth, f is the kelly fraction to be considered
+    k = minimize_util(p1, r1, r2)
+    wealth, wealth_k, wealth_fk = [wealth_1] * n_trades, [wealth_1] * n_trades, [wealth_1] * n_trades
+    periods = np.linspace(0, n_trades-1, num=n_trades)
+    outcomes = binom.rvs(1, p1, size = n_trades)
+    for i in range(1, len(outcomes)):
+      if outcomes[i] == 0:
+        wealth[i] = wealth[i-1] * (1-r2)
+        wealth_k[i] = wealth_k[i-1] * k * (1-r2) + wealth_k[i-1] * (1-k)
+        wealth_fk[i] = wealth_fk[i-1] * f*k * (1-r2) + wealth_fk[i-1] * (1-f*k)
+      else:
+        wealth[i] = wealth[i-1] * (1+r1)
+        wealth_k[i] = wealth_k[i-1] * k * (1+r1) + wealth_k[i-1] * (1-k)
+        wealth_fk[i] = wealth_fk[i-1] * f*k * (1+r1) + wealth_fk[i-1] * (1-f*k)
+    df = pd.DataFrame(data = {'full_wealth':wealth,
+                            'kelly_wealth':wealth_k,
+                            f'{param5}_kelly_wealth':wealth_fk,
+                            'periods':periods})
+    return df
+
+  df = wealth(param3, param1, param2, 75, 1, .75) # make this selectable in the streamlit application
+  dfm = df.melt('periods', var_name = 'wealth_fraction', value_name = 'wealth')
+  st.subheader("Expected Wealth w/ Different Kelly Fractions")
+  fig2, ax2 = plt.subplots()
+  sns.lineplot(data=dfm, x='periods', y='wealth', hue='wealth_fraction', ax=ax2)
+  plt.xlabel("Investment Periods")
+  plt.ylabel("Expected Wealth")
+  st.pyplot(fig2)
 
 with tab2:
   # Sidebar for user input in Tab 1
-  st.sidebar.header("Kelly Fraction Simulation Parameters")
-
-  sp500_top50 = [
-      'AAPL', 'MSFT', 'AMZN', 'GOOGL', 'GOOG', 'BRK-B', 'NVDA', 'TSLA', 'META',
-      'UNH', 'JNJ', 'V', 'XOM', 'WMT', 'JPM', 'MA', 'PG', 'HD', 'BAC', 'PFE',
-      'ABBV', 'KO', 'DIS', 'PEP', 'NFLX', 'CSCO', 'NKE', 'LLY', 'MCD', 'VZ',
-      'MRK', 'TMO', 'CVX', 'ABT', 'CMCSA', 'DHR', 'AVGO', 'COST', 'ACN', 'NEE',
-      'WFC', 'LIN', 'TXN', 'ADBE', 'MDT', 'HON', 'UNP', 'ORCL', 'PM'
-  ]
-  # Ticker selection from sp500_top50
-  tickers = st.sidebar.multiselect("Select Tickers",
-                                   sp500_top50,
-                                   default=["AAPL", "MSFT", "GOOGL"])
 
   selected_stock = tickers[0]
-  n_simulations = st.sidebar.slider("Number of Simulations",
+  n_simulations = stslider("Number of Simulations",
                                     min_value=100,
                                     max_value=5000,
                                     value=1000)
-  n_months = st.sidebar.slider("Number of Months",
+  n_months = st.slider("Number of Months",
                                min_value=10,
                                max_value=120,
                                value=30)
-  n_steps = st.sidebar.slider("Number of Kelly Steps",
+  n_steps = st.slider("Number of Kelly Steps",
                               min_value=5,
                               max_value=50,
                               value=25)
 
-  run_simulation = st.sidebar.button("Run Simulation")
+  run_simulation = st.button("Run Simulation")
 
   if run_simulation:
     # Download the selected stock data
@@ -173,16 +217,16 @@ with tab2:
 
 with tab3:
   # Sidebar for user input in Tab 2
-  st.sidebar.header("Optimal Portfolio Parameters")
+  st.header("Optimal Portfolio Parameters")
 
 
   ticker1 = tickers[0]
   ticker2 = tickers[1]
   ticker3 = tickers[2]
 
-  start_date = st.sidebar.date_input("Start Date",
+  start_date = st.date_input("Start Date",
                                      value=pd.to_datetime('2013-01-01'))
-  end_date = st.sidebar.date_input("End Date",
+  end_date = st.date_input("End Date",
                                    value=pd.to_datetime('2023-04-30'))
 
   run_analysis = st.sidebar.button("Run Analysis")
